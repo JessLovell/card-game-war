ba7259724653177228dd274b7c814001a465d72a
RazorWire13 <baneblade13@gmail.com>
2018-08-27 15:19:40 -0700

    Basic HTML structure added to index.html and custom fonts added to CSS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function getInstructions () {
+  document.getElementById('instructDropdown').classList.toggle('show');
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName('dropdown-content');
+    for (var i = 0; i< dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
