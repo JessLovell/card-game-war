6f3f79ed57f9d4d8d63c524694a153422abe2fb7
Patrick McNamee <pjmcnamee@comcast.net>
2018-08-28 11:25:57 -0700

    Fixed War and Added Tracking

--- js/war-game.js
+++ js/war-game.js
@@ -10,6 +10,9 @@ var userHoldPile = [];
 
 var gamePlayField = [];
 
+var userCardTracker = 0;
+var computerCardTracker = 0;
+
 
 function DeckCreator (name, value){
   this.name = name;
@@ -74,6 +77,7 @@ function compareCards (){
 
     computerPlayerDeck.splice(0, 1);
     userPlayerDeck.splice(0, 1);
+    computerCardTracker += 2;
   }
   else if (userPlayerDeck[0].value > computerPlayerDeck[0].value){
     userHoldPile.push(computerPlayerDeck[0]);
@@ -81,6 +85,7 @@ function compareCards (){
 
     computerPlayerDeck.splice(0, 1);
     userPlayerDeck.splice(0, 1);
+    userCardTracker += 2;
   } else {
     war();
   }
@@ -93,46 +98,74 @@ function war (){
     if (computerPlayerDeck[4].value > userPlayerDeck[4].value){
       computerHoldPile.push(computerPlayerDeck[0,1,2,3,4]);
       computerHoldPile.push(userPlayerDeck[0,1,2,3,4]);
+      computerPlayerDeck.splice(0,5);
+      userPlayerDeck.splice(0,5);
+      computerCardTracker += 10;
     }
   } else {
     if (computerPlayerDeck[4].value < userPlayerDeck[4].value){
       userHoldPile.push(userPlayerDeck[0,1,2,3,4]);
       userHoldPile.push(computerPlayerDeck[0,1,2,3,4]);
+      computerPlayerDeck.splice(0,5);
+      userPlayerDeck.splice(0,5);
+      userCardTracker += 10;
     }
   }
   if (computerPlayerDeck.length > 4 && userPlayerDeck.length > 4){
     if (computerPlayerDeck[3].value > userPlayerDeck[3].value){
       computerHoldPile.push(computerPlayerDeck[0,1,2,3]);
       computerHoldPile.push(userPlayerDeck[0,1,2,3]);
+      computerPlayerDeck.splice(0,4);
+      userPlayerDeck.splice(0,4);
+      computerCardTracker += 8;
     } else {
       if (computerPlayerDeck[3].value < userPlayerDeck[3].value){
         userHoldPile.push(userPlayerDeck[0,1,2,3]);
         userHoldPile.push(computerPlayerDeck[0,1,2,3]);
+        computerPlayerDeck.splice(0,4);
+        userPlayerDeck.splice(0,4);
+        userCardTracker += 8;
       }
     }
   }if (computerPlayerDeck.length > 3 && userPlayerDeck.length > 3){
     if (computerPlayerDeck[2].value > userPlayerDeck[2].value){
       computerHoldPile.push(computerPlayerDeck[0,1,2]);
       computerHoldPile.push(userPlayerDeck[0,1,2]);
+      computerPlayerDeck.splice(0,3);
+      userPlayerDeck.splice(0,3);
+      computerCardTracker += 6;
     } else {
       if (computerPlayerDeck[2].value < userPlayerDeck[2].value){
         userHoldPile.push(userPlayerDeck[0,1,2]);
         userHoldPile.push(computerPlayerDeck[0,1,2]);
+        computerPlayerDeck.splice(0,3);
+        userPlayerDeck.splice(0,3);
+        userCardTracker += 6;
       }
     }
   }if (computerPlayerDeck.length > 2 && userPlayerDeck.length > 2){
     if (computerPlayerDeck[1].value > userPlayerDeck[1].value){
       computerHoldPile.push(computerPlayerDeck[0,1]);
       computerHoldPile.push(userPlayerDeck[0,1]);
+      computerPlayerDeck.splice(0,2);
+      userPlayerDeck.splice(0,2);
+      computerCardTracker += 4;
     } else {
       if (computerPlayerDeck[1].value < userPlayerDeck[1].value){
         userHoldPile.push(userPlayerDeck[0,1]);
         userHoldPile.push(computerPlayerDeck[0,1]);
+        computerPlayerDeck.splice(0,2);
+        userPlayerDeck.splice(0,2);
+        userCardTracker += 4;
       }
     }
   }if (computerPlayerDeck.length > 1 && userPlayerDeck.length > 1){
     computerHoldPile.push(computerPlayerDeck[0]);
     userHoldPile.push(userPlayerDeck[0]);
+    computerPlayerDeck.splice(0,1);
+    userPlayerDeck.splice(0,1);
+    computerCardTracker++;
+    userCardTracker++;
     return;
   }
 }
